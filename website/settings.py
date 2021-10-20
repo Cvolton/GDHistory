a9c177b33e77834ece71d042cb4362edff5303f4
Cvolton <cvolton@cvolton.eu>
2021-10-20 11:06:16 +0200

    Add history to installed apps

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'history.apps.HistoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
