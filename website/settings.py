b9c7c7afdde406af59b6e3e26b83f173145cea2b
Cvolton <cvolton@cvolton.eu>
2021-10-20 11:05:57 +0200

    Change database to MySQL

--- website/settings.py
+++ website/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gdhistory',
+        'USER': 'gdhistory',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
