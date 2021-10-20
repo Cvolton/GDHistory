5ab7291291ef8bb05bcffc03b94d674ea43023e2
Cvolton <cvolton@cvolton.eu>
2021-10-20 20:28:05 +0200

    Move parser.py to ccUtils.py

--- history/ccUtils.py
+++ history/ccUtils.py
@@ -4,20 +4,19 @@ import base64
 import gzip
 
 def get_game_manager_bytes():
-	game_manager_file = open(os.path.expanduser('~/testdata/gdhistory/CCGameManager_21.dat'), "rb")
+	game_manager_file = open(os.path.expanduser('~/testdata/gdhistory/CCGameManager_decodetest.dat'), "rb")
 	game_manager_bytes = game_manager_file.read()
 	game_manager_file.close()
-	return game_manager_bytes
+	return bytearray(game_manager_bytes)
 
 def xor_game_manager_if_needed(game_manager_bytes):
-	game_manager_bytes = bytearray(get_game_manager_bytes())
+	game_manager_bytes = get_game_manager_bytes()
 	if game_manager_bytes[:5] == b'C?xBJ':
 		i = 0
 		for byte in game_manager_bytes:
 			game_manager_bytes[i] = byte ^ 11
 			i+=1
-		#return bytes(a ^ b for a, b in zip(var, key))
-	return bytes(game_manager_bytes)
+	return game_manager_bytes
 
 def ungzip_if_needed(game_manager_bytes):
 	if game_manager_bytes[:5] == b'H4sIA':
@@ -46,16 +45,25 @@ def remove_invalid_characters(game_manager_bytes):
 	game_manager_bytes = game_manager_bytes.replace(b'#',b'@@hash@@')
 	return game_manager_bytes
 
+gmb = get_game_manager_bytes()
+gmb = xor_game_manager_if_needed(gmb)
+gmb = ungzip_if_needed(gmb)
+gmb = robtop_plist_to_plist(gmb)
+gmb = remove_invalid_characters(gmb)
 
-gmb = remove_invalid_characters(robtop_plist_to_plist(ungzip_if_needed(xor_game_manager_if_needed(get_game_manager_bytes()))))
-
-f = open(os.path.expanduser('~/testdata/gdhistory/CCGameManager_decodetest.dat'), "wb")
-f.write(gmb)
-f.close()
+f = open(os.path.expanduser('~/testdata/gdhistory/CCGameManager_decodetest2.dat'), "wb")
+#f.write(gmb)
+#f.close()
 
 game_manager = plistlib.loads(gmb)
 glm_03 = game_manager['GLM_03']
 
 for level, data in glm_03.items():
-	print(level)
+	print(f"{level} - {data['k2']}")
 	#print(data)
+
+print(game_manager['GJA_002'])
+game_manager['GJA_002'] = ''
+print(game_manager['GJA_002'])
+plistlib.dump(game_manager, f)
+f.close()
