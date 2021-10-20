4d4122d0850e960243b71bbd6b99dadf1ae35c23
Cvolton <cvolton@cvolton.eu>
2021-10-20 20:40:58 +0200

    Register models in admin panel

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import HistoryUser, SaveFile, GetGJLevelsResponse, Level, LevelRecord
 
 # Register your models here.
+admin.site.register(HistoryUser)
+admin.site.register(SaveFile)
+admin.site.register(GetGJLevelsResponse)
+admin.site.register(Level)
+admin.site.register(LevelRecord)
