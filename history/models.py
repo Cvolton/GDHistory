33896cf58881d3d880decab552ecd7a2ab400682
Cvolton <cvolton@cvolton.eu>
2021-10-20 10:53:15 +0200

    Update LevelRecord model to include foreign keys

--- history/models.py
+++ history/models.py
@@ -21,6 +21,11 @@ class SaveFile(models.Model):
 	is_processed = models.BooleanField(default=False)
 	#also raw save file with password stripped out stored on the side in a file
 
+class GetGJLevelsResponse(models.model):
+	saved = models.DateTimeField(default=datetime.now)
+	unprocessed_post_parameters = models.JSONField()
+
+
 class Level(models.Model):
 	online_id = models.IntegerField(db_index=True)
 	is_public = models.BooleanField() #this is to prevent leaking unlisted levels publicly
@@ -43,7 +48,21 @@ class LevelRecord(models.Model):
 		db_index=True,
 	)
 
-	unprocessed_data = models.JSONField()
+	save_file = models.ForeignKey(
+		SaveFile,
+		on_delete=models.CASCADE,
+		blank=True, null=True,
+		db_index=True,
+	)
+
+	get_gj_levels_response = models.ForeignKey(
+		GetGJLevelsResponse,
+		on_delete=models.CASCADE,
+		blank=True, null=True,
+		db_index=True,
+	)
+
+	unprocessed_data = models.JSONField() #this field should only be used for archival purposes, do not pull data from this directly in production
 
 	#also levelstring stored on the side
 	#and raw server response for download type records stored on the side
