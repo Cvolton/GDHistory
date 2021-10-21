ac58e7a26b9a49e6d6ba659932e506e5295fc24b
Cvolton <cvolton@cvolton.eu>
2021-10-21 02:07:13 +0200

    Add basic level info view

--- history/urls.py
+++ history/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('level/<online_id>', views.view_level, name='level'),
 ]
--- history/views.py
+++ history/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Level, LevelRecord
 from . import ccUtils
 
 def index(request):
 	ccUtils.test()
 	return HttpResponse("welcome")
+
+def view_level(request, online_id=None):
+	level_records = LevelRecord.objects.filter(level__online_id=online_id).prefetch_related('level')
+	context = {'level_records': level_records}
+	return render(request, 'level.html', context)
