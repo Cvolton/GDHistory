475744a33f8b0344c5d8c779aa18760abdbb76ba
Cvolton <cvolton@cvolton.eu>
2021-10-22 23:40:21 +0200

    Improved level view page

--- history/views.py
+++ history/views.py
@@ -10,5 +10,5 @@ def index(request):
 
 def view_level(request, online_id=None):
 	level_records = LevelRecord.objects.filter(level__online_id=online_id).prefetch_related('level')
-	context = {'level_records': level_records}
+	context = {'level_records': level_records, 'online_id': online_id}
 	return render(request, 'level.html', context)
