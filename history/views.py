56bcdb2760b8aa72db2646d365b1811294ca6a06
Cvolton <cvolton@cvolton.eu>
2021-10-26 23:37:36 +0200

    Basic Bootstrap design attempts

--- history/views.py
+++ history/views.py
@@ -5,8 +5,7 @@ from .models import Level, LevelRecord
 from . import ccUtils, serverUtils
 
 def index(request):
-	ccUtils.test()
-	return HttpResponse("welcome")
+	return render(request, 'index.html')
 
 def view_level(request, online_id=None):
 	level_records = LevelRecord.objects.filter(level__online_id=online_id).prefetch_related('level')
