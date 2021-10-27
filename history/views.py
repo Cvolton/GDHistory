f5342145e0457427617c6a698031e56bbba8218d
Cvolton <cvolton@cvolton.eu>
2021-10-27 13:45:24 +0200

    Add error handling for non-existent levels

--- history/views.py
+++ history/views.py
@@ -10,6 +10,9 @@ def index(request):
 
 def view_level(request, online_id=None):
 	level_records = LevelRecord.objects.filter(level__online_id=online_id).prefetch_related('level').prefetch_related('save_file')
+	if len(level_records) == 0:
+		return render(request, 'error.html', {'error': 'Level not found in our database'})
+
 	context = {'level_records': level_records, 'level': level_records[0].level, 'online_id': online_id}
 
 	serverUtils.download_level(online_id)
