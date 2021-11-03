cb519363744069d49564e87cc0f860143b7cd824
Cvolton <cvolton@cvolton.eu>
2021-11-04 00:21:38 +0100

    Difficulty names

---
+++ history/templatetags/tags.py
@@ -0,0 +1,35 @@
+import datetime
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def difficulty(rating_sum, rating, demon, auto):
+	if auto:
+		return "Auto"
+
+	if demon:
+		return "Demon"
+
+	if rating == 0:
+		return "N/A"
+
+	diff = rating_sum / rating
+
+	if diff < 0:
+		return "N/A"
+
+	if diff < 1.5:
+		return "Easy"
+
+	if diff < 2.5:
+		return "Normal"
+
+	if diff < 3.5:
+		return "Hard"
+
+	if diff < 4.5:
+		return "Harder"
+
+	if diff < 5.5:
+		return "Insane"
