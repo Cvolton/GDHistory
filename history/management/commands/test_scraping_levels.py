d962dcb0c6e9db8fb153313aea78b543268e6806
Cvolton <cvolton@cvolton.eu>
2021-12-02 15:29:11 +0100

    Update scraping test script to include multiple level types

--- history/management/commands/test_scraping_levels.py
+++ history/management/commands/test_scraping_levels.py
@@ -1,4 +1,5 @@
 import history.serverUtils
+from history.constants import GetLevelTypes
 
 import os
 from datetime import datetime
@@ -8,4 +9,10 @@ class Command(BaseCommand):
 	help = 'Adds save files from a pre-defined directory'
 
 	def handle(self, *args, **options):
-		history.serverUtils.get_first_level_pages(11, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.LIKED, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.DOWNLOADED, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.TRENDING, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.RECENT, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.FEATURED, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.AWARDED, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.HALL_OF_FAME, 10)
