fa60bf50a953b120b757e5ad5a1c84e71de07a9f
Cvolton <cvolton@cvolton.eu>
2021-12-31 00:51:17 +0100

    Update scraping test

--- history/management/commands/test_scraping_levels.py
+++ history/management/commands/test_scraping_levels.py
@@ -9,12 +9,11 @@ class Command(BaseCommand):
 	help = 'Test command'
 
 	def handle(self, *args, **options):
-		"""history.serverUtils.get_first_level_pages(GetLevelTypes.LIKED, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.LIKED, 10)
 		history.serverUtils.get_first_level_pages(GetLevelTypes.DOWNLOADED, 10)
 		history.serverUtils.get_first_level_pages(GetLevelTypes.TRENDING, 10)
+		history.serverUtils.get_first_level_pages(GetLevelTypes.MAGIC, 10)
 		history.serverUtils.get_first_level_pages(GetLevelTypes.RECENT, 10)
-		history.serverUtils.get_first_level_pages(GetLevelTypes.FEATURED, 10)
 		history.serverUtils.get_first_level_pages(GetLevelTypes.AWARDED, 10)
-		history.serverUtils.get_first_level_pages(GetLevelTypes.HALL_OF_FAME, 10)"""
 
-		history.serverUtils.get_level_pages_from(GetLevelTypes.AWARDED, 10)
+		#history.serverUtils.get_level_pages_from(GetLevelTypes.AWARDED, 1043)
