c476b3cea87d24624038fa479d8725aeb3a4665d
Cvolton <cvolton@cvolton.eu>
2022-01-13 05:01:00 +0100

    Update command to set is_public to True for all levels with gameVersion <= 20

--- history/management/commands/update_is_public.py
+++ history/management/commands/update_is_public.py
@@ -3,12 +3,16 @@ import history.utils
 import json
 
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
 
 class Command(BaseCommand):
 	help = 'Exports server responses as JSON'
 
 	def handle(self, *args, **options):
-		records = LevelRecord.objects.filter(record_type=LevelRecord.RecordType.GET).exclude(level__is_public=True).prefetch_related('level')
+		"""Note that this expects gameVersion to be set correctly, which isn't guaranteed in the real world.
+		There is at least one version of GD 2.1, which had gameVersion set to 20 for all uploaded levels.
+		The amount of levels uploaded like this is so low, that this is	not an edge case worth handling."""
+		records = LevelRecord.objects.filter(Q(record_type=LevelRecord.RecordType.GET) | Q(game_version__lte=20)).exclude(level__is_public=True).prefetch_related('level')
 		record_count = records.count()
 		i = 1
 		for record in records:
