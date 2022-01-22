24b9ec60a3cf2c3783bd061b50dece08adaa3a67
Cvolton <cvolton@cvolton.eu>
2022-01-22 04:32:20 +0100

    Add command for exporting a task containing levels without levelString

---
+++ history/management/commands/export_levels_without_levelstring.py
@@ -0,0 +1,39 @@
+from history.models import Level
+from history.constants import MiscConstants
+import history.utils
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+
+class Command(BaseCommand):
+	help = 'Creates a downloader task with undownloaded levels'
+
+	def handle(self, *args, **options):
+		data_path = history.utils.get_data_path()
+		levels = Level.objects.exclude(is_deleted=True).prefetch_related('levelrecord_set')
+		level_count = levels.count()
+		levels_to_export = []
+		i = 0
+		for level in levels:
+			i += 1
+			string_count = level.levelrecord_set.exclude(level_string=None).count()
+			if string_count > 0:
+				continue
+			print(f"{i} / {level_count} - {level.online_id} {string_count}")
+			levels_to_export.append(level.online_id)
+
+		task_json = {
+			"endpoint": "downloadGJLevel22",
+			"parameters": {
+				"inc": 0,
+				"extras": 1
+			},
+			"levelList": levels_to_export
+		}
+
+		f = open(f"{data_path}/Exports/LevelTask.json", "w")
+		json.dump(task_json, f)
+		f.close()
+
+		print("Done")
