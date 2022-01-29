d51c5bb6e5b3fede8ec276cd09dfcfde721e71d0
Cvolton <cvolton@cvolton.eu>
2022-01-30 00:31:17 +0100

    Update update_is_public

--- history/management/commands/update_is_public.py
+++ history/management/commands/update_is_public.py
@@ -5,13 +5,14 @@ import json
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
+from django.db import connection
 
 class Command(BaseCommand):
 	help = 'Exports server responses as JSON'
 
 	def handle(self, *args, **options):
 
-		records = LevelRecord.objects.filter( Q(level__online_id__lt=MiscConstants.FIRST_2_1_LEVEL) | Q(record_type=LevelRecord.RecordType.GET) | ( Q(record_type=LevelRecord.RecordType.DOWNLOAD) & Q(server_response__created__gte="2021-11-24 02:10:00+00:00") ) ).exclude(level__is_public=True).prefetch_related('level')
+		records = LevelRecord.objects.filter( Q(level__online_id__lt=MiscConstants.FIRST_2_1_LEVEL) | Q(record_type=LevelRecord.RecordType.GET) | ( Q(record_type=LevelRecord.RecordType.DOWNLOAD) & Q(server_response__created__gte="2021-11-24 02:10:00+00:00") ) ).filter( Q(level__is_public=None) | Q(level__is_public=False) ).prefetch_related('level')
 		record_count = records.count()
 		i = 1
 		for record in records:
