16c0913f225e6b9f94d6dfe2f99038fd9078cd72
Cvolton <cvolton@cvolton.eu>
2022-02-08 21:44:46 +0100

    Add reprocess_everything command

---
+++ history/management/commands/reprocess_everything.py
@@ -0,0 +1,28 @@
+from history.models import LevelRecord, SongRecord, SaveFile
+import history.ccUtils
+
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+	help = 'Deletes all LevelRecords'
+
+	def handle(self, *args, **options):
+		print("Clearing level records")
+		level_records = LevelRecord.objects.all().delete()
+
+		print("Clearing song records")
+		song_records = SongRecord.objects.all().delete()
+
+		print("Flagging save files")
+		save_files = SaveFile.objects.all()
+		for file in save_files:
+			file.is_processed = False
+			file.save()
+
+		print("Processing save files")
+		for file in save_files:
+			#TODO: handle failures properly
+			try:
+				history.ccUtils.process_save_file(file.pk)
+			except:
+				pass
