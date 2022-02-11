8f8ea7ffa2104d0e4a2aa41143fce88acc1db9ff
Cvolton <cvolton@cvolton.eu>
2022-02-11 20:25:43 +0100

    Optimize update_is_public for low RAM

--- history/management/commands/update_is_public.py
+++ history/management/commands/update_is_public.py
@@ -14,11 +14,10 @@ class Command(BaseCommand):
 
 		records = LevelRecord.objects.filter( Q(level__online_id__lt=MiscConstants.FIRST_2_1_LEVEL) | Q(record_type=LevelRecord.RecordType.GET) | ( Q(record_type=LevelRecord.RecordType.DOWNLOAD) & Q(server_response__created__gte="2021-11-24 02:10:00+00:00") ) ).filter( Q(level__is_public=None) | Q(level__is_public=False) ).prefetch_related('level')
 		record_count = records.count()
-		i = 1
-		for record in records:
+		for i in range(0,record_count):
+			record = records[0:1]
+			record = record[0]
 			print(f"{i} / {record_count} - Updating {record.level.online_id}")
 			record.level.set_public(True)
-
-			i += 1
 
 		print("Done")
