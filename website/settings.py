26586491df9f82e739bb4f48e82d130a96cc58b5
Cvolton <cvolton@cvolton.eu>
2022-02-11 03:00:58 +0100

    Fix typo

--- website/settings.py
+++ website/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.getenv('STATIC_ROOT','static'),
+STATIC_ROOT = os.getenv('STATIC_ROOT','/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
