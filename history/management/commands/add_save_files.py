65c50739a1da02935347fc7eca0a278165695c9d
Cvolton <cvolton@cvolton.eu>
2022-02-11 03:59:47 +0100

    Update save file import command

--- history/management/commands/add_save_files.py
+++ history/management/commands/add_save_files.py
@@ -9,14 +9,15 @@ class Command(BaseCommand):
 	help = 'Adds save files from a pre-defined directory'
 
 	def handle(self, *args, **options):
-		directory = '/run/media/cvolton/DATA/testdata/save_import/'
+		data_path = history.utils.get_data_path()
+		directory = f"{data_path}/Imports/SaveFile/"
 		for filename in os.listdir(directory):
 			game_manager_path = f"{directory}/{filename}/CCGameManager.dat"
 			if not os.path.exists(game_manager_path):
 				print("Save file {game_manager_path} not found")
 				continue
 			f = open(game_manager_path, "rb")
-			parsed_date = datetime.strptime(filename, '%d.%m.%Y')
+			parsed_date = datetime.strptime(filename, '%Y-%m-%d')
 			print(parsed_date)
 			if SaveFile.objects.filter(created=parsed_date).exists():
 				print(f"{parsed_date} already exists")
