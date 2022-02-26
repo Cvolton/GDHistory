0afcc1eb7234e155ce70792eaf44fc6e7c9977b2
Cvolton <cvolton@cvolton.eu>
2022-02-26 05:29:59 +0100

    Create revalidate_cache_individual command

---
+++ history/management/commands/revalidate_cache_individual.py
@@ -0,0 +1,20 @@
+from history.models import Level, Song
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+
+class Command(BaseCommand):
+	help = 'Revalidates level cache'
+
+	def add_arguments(self, parser):
+		parser.add_argument('levels', nargs='+')
+
+	def handle(self, *args, **options):
+		total = len(options['levels'])
+		for i, level in enumerate(options['levels']):
+			levels = Level.objects.all().prefetch_related('levelrecord_set__save_file').prefetch_related('levelrecord_set__level_string').filter(online_id=level)
+			for level_object in levels:
+				print(f"{i} / {total} - Updating {level_object.online_id}")
+				level_object.revalidate_cache()
+
+		print("Done")
