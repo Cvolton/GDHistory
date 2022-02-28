7f37efd5097f2c049680ce2c8931babef127c212
Cvolton <cvolton@cvolton.eu>
2022-02-28 18:25:03 +0100

    Update cached fields to not cause sorting issues

--- history/management/commands/update_cached_fields.py
+++ history/management/commands/update_cached_fields.py
@@ -1,4 +1,4 @@
-from history.models import LevelRecord
+from history.models import LevelRecord, Level
 from history.constants import MiscConstants
 import history.utils
 import json
@@ -9,7 +9,7 @@ from django.db.models import Q
 class Command(BaseCommand):
 	help = 'Exports server responses as JSON'
 
-	def do_updating(self, records):
+	def do_is_public_updating(self, records):
 		record_count = records.count()
 		i = 1
 		for record in records:
@@ -20,9 +20,24 @@ class Command(BaseCommand):
 
 		LevelRecord.objects.bulk_update(records, ['cache_is_public'])
 
+	def do_none_updating(self, records):
+		record_count = records.count()
+		i = 1
+		for record in records:
+			print(f"{i} / {record_count} - Updating {record.online_id}")
+			record.cache_downloads = record.cache_downloads if record.cache_downloads is not None else 0
+			record.cache_likes = record.cache_likes if record.cache_likes is not None else 0
+			record.cache_stars = record.cache_stars if record.cache_stars is not None else 0
+			#record.save()
+			i += 1
+
+		Level.objects.bulk_update(records, ['cache_stars', 'cache_likes', 'cache_downloads'])
+
 	def handle(self, *args, **options):
-		self.do_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=False).exclude(level__is_public=False))
-		self.do_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=True).exclude(level__is_public=True))
-		self.do_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=None).exclude(level__is_public=None))
+		self.do_is_public_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=False).exclude(level__is_public=False))
+		self.do_is_public_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=True).exclude(level__is_public=True))
+		self.do_is_public_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=None).exclude(level__is_public=None))
+
+		self.do_none_updating(Level.objects.filter( Q(cache_downloads=None) | Q(cache_likes=None) | Q(cache_stars=None) ))
 
 		print("Done")
