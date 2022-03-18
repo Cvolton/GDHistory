6f0b5a149a5bf33343baacb133efb595fc4f873e
Cvolton <cvolton@cvolton.eu>
2022-03-19 00:40:35 +0100

    Add status prints to export_destroyed_users

--- history/management/commands/export_destroyed_users.py
+++ history/management/commands/export_destroyed_users.py
@@ -10,13 +10,16 @@ class Command(BaseCommand):
 	help = 'Exports server responses as JSON'
 
 	def handle(self, *args, **options):
-		records = GDUser.objects.filter(gduserrecord__record_type=LevelRecordType.GET, gduserrecord__username=None).prefetch_related('gduserrecord_set').distinct()
+		print("_begin")
+		records = GDUser.objects.filter(gduserrecord__record_type=LevelRecordType.GLM_03, gduserrecord__username=None).prefetch_related('gduserrecord_set').distinct()
 		record_count = records.count()
 		batch_size = 2500
 		batch_count = math.ceil(record_count/2500)
 		for i in range(0,batch_count):
+			print(f"_batch {i}")
 			users_small = records[i*batch_size:(i+1)*batch_size]
 			for user in users_small:
+				print(f"_user {user.online_id}")
 				record_list = user.gduserrecord_set.exclude( Q(username='-') | Q(username=None) ).order_by('-cache_created')
 				non_player_record = record_list.exclude(username='Player')[:1]
 				if len(non_player_record) > 0:
