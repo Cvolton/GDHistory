d6f2a74cd77886f1824ea716fbc25b1fd510a474
Cvolton <cvolton@cvolton.eu>
2022-03-19 00:12:42 +0100

    Optimize export_destroyed_users for low RAM

--- history/management/commands/export_destroyed_users.py
+++ history/management/commands/export_destroyed_users.py
@@ -10,14 +10,18 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		records = GDUser.objects.filter(gduserrecord__record_type=LevelRecordType.GET, gduserrecord__username=None).prefetch_related('gduserrecord_set').distinct()
-		for user in records:
-			record_list = user.gduserrecord_set.exclude( Q(username='-') | Q(username=None) ).order_by('-cache_created')
-			non_player_record = record_list.exclude(username='Player')[:1]
-			if len(non_player_record) > 0:
-				print(f"{user.online_id} - {non_player_record[0].username}")
-			else:
-				record_list = record_list[:1]
-				if len(record_list) > 0:
-					print(f"{user.online_id} - {record_list[0].username}")
+		batch_size = 2500
+		batch_count = math.ceil(level_count/2500)
+		for i in range(0,batch_count):
+			users_small = records[i*batch_size:(i+1)*batch_size]
+			for user in users_small:
+				record_list = user.gduserrecord_set.exclude( Q(username='-') | Q(username=None) ).order_by('-cache_created')
+				non_player_record = record_list.exclude(username='Player')[:1]
+				if len(non_player_record) > 0:
+					print(f"{user.online_id} - {non_player_record[0].username}")
+				else:
+					record_list = record_list[:1]
+					if len(record_list) > 0:
+						print(f"{user.online_id} - {record_list[0].username}")
 
 		print("Done")
