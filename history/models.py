f0b35a349deb8accdb51d5c0a88e4f566a3508d2
Cvolton <cvolton@cvolton.eu>
2022-07-05 15:29:20 +0200

    Fix null users saving

--- history/models.py
+++ history/models.py
@@ -528,7 +528,7 @@ class LevelRecord(models.Model):
 		
 		user_object = utils.get_user_object(self.user_id)
 		user_record = utils.create_user_record(user_object, self.account_id, self.username, record_date, self.server_response, self.save_file, self.record_type)
-		user_object.update_with_record(user_record)
+		if user_object is not None: user_object.update_with_record(user_record)
 
 		self.real_user_record = user_record
 		self.save()
