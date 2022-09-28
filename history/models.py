397490525c5400dc10a032e1bd8f14b592893fe9
Cvolton <cvolton@cvolton.eu>
2022-09-28 23:34:22 +0200

    Add deleted wasrated key

--- history/models.py
+++ history/models.py
@@ -384,6 +384,7 @@ class Level(models.Model):
 			models.Index(fields=['cache_search_available', 'cache_level_string_available'], name='playable'),
 			models.Index(fields=['cache_search_available', 'is_deleted'], name='deleted'),
 			models.Index(fields=['cache_search_available', 'is_deleted', 'cache_level_string_available'], name='deleted_playable'),
+			models.Index(fields=['cache_search_available', 'is_deleted', 'cache_max_stars'], name='deleted_wasrated'),
 
 			models.Index(fields=['cache_search_available', 'cache_stars', 'cache_max_stars'], name='wasrated_staronly'),
 			models.Index(fields=['cache_search_available', 'is_deleted', 'cache_stars'], name='deleted_staronly'),
