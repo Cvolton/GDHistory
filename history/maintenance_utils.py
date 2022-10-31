b85604eccc37aa086f1b79f94668db6ad3e1376d
Cvolton <cvolton@cvolton.eu>
2022-10-31 16:56:31 +0100

    Update user whitelist

--- history/maintenance_utils.py
+++ history/maintenance_utils.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from django.db import connection
 
 def update_is_public():
-	user_whitelist = [21297937, 16, 11094602]
+	user_whitelist = [21297937, 16, 11094602, 20417551]
 	records = LevelRecord.objects.filter( Q(level__cache_user_id__in=user_whitelist) | Q(level__cache_stars__gt=0) | Q(level__cache_downloads__gte=1000) | Q(level__online_id__lt=MiscConstants.FIRST_2_1_LEVEL) | Q(record_type=LevelRecordType.GET) | ( Q(record_type=LevelRecordType.DOWNLOAD) & Q(server_response__created__gte="2021-11-24 02:10:00+00:00") ) ).filter( Q(level__is_public=None) | Q(level__is_public=False) ).prefetch_related('level')
 	record_count = records.count()
 	for i in range(0,record_count):
