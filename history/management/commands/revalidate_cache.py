7b674751657f950e2927096979a64aed30ae7f59
Cvolton <cvolton@cvolton.eu>
2022-12-18 02:31:57 +0100

    Change order of cache revalidation

--- history/management/commands/revalidate_cache.py
+++ history/management/commands/revalidate_cache.py
@@ -6,12 +6,18 @@ from django.db.models import Q
 class Command(BaseCommand):
 	help = 'Revalidates level cache'
 
-	def handle(self, *args, **options):
-		levels = Level.objects.filter(cache_needs_revalidation=True).prefetch_related('levelrecord_set__save_file').prefetch_related('levelrecord_set__level_string')
+	def recalculate_levels(self, levels):
 		level_count = levels.count()
 		for i in range(0,level_count):
 			level = levels[0:1][0]
 			print(f"{i} / {level_count} - Updating {level.online_id}")
 			level.revalidate_cache()
+
+	def handle(self, *args, **options):
+		levels = Level.objects.filter(cache_needs_revalidation=True).prefetch_related('levelrecord_set__save_file').prefetch_related('levelrecord_set__level_string')
+		print("pass 1")
+		self.recalculate_levels(levels.filter(cache_submitted=None))
+		print("pass 2")
+		self.recalculate_levels(levels.order_by('cache_submitted'))
 
 		print("Done")
