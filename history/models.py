2f74994c639ba0905960333f201ad2ded9611024
Cvolton <cvolton@cvolton.eu>
2023-11-16 23:24:39 +0100

    remove test model entry

--- history/models.py
+++ history/models.py
@@ -27,7 +27,6 @@ class HistoryUser(models.Model):
         on_delete=models.CASCADE,
         db_index=True,
     )
-    test = models.CharField(max_length=255)
     def __str__(self):
         return self.user.username
 
