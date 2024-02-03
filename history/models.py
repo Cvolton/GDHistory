1c698a40dbdd2e9d03120858c9736d9251b3210e
Cvolton <cvolton@cvolton.eu>
2024-02-03 09:20:27 +0100

    reduce amount of db hits for revalidate cache

--- history/models.py
+++ history/models.py
@@ -618,12 +618,12 @@ class Level(models.Model):
 		highest_downloads_with_levelstring = 0
 		highest_downloads_with_levelstring_record = None
 
-		for record in self.levelrecord_set.filter(cache_is_dupe=False).prefetch_related('real_user_record__user').order_by('downloads'):
+		for record in self.levelrecord_set.filter(cache_is_dupe=False).order_by('downloads'):
 			record.upgrade_data()
 			if not record.real_user_record:
 				record.create_user()
 			#name, rating_sum, ratings, demon, auto, stars, version, real_user_record, game_version, levelstring
-			current_record_string = f"{record.level_name or 0}, {record.rating or 0}, {record.rating_sum or 0}, {record.auto or 0}, {record.demon or 0}, {record.stars or 0}, {record.demon_type or 0}, {record.level_version or 0}, {record.real_user_record.get_serialized_base() if record.real_user_record else (record.username or 0)}, {record.game_version or 0}, {record.level_string or 0}, {record.coins or 0}, {record.description or 0}, {record.song or 0}, {record.official_song or 0}, {record.feature_score or 0}, {record.epic or 0}, {record.password or 0}, {record.two_player or 0}, {record.objects_count or 0}, {record.extra_string or 0}, {record.original or 0}, {record.daily_id or 0}, {record.timestamp or 0}, {record.song_ids}, {record.sfx_ids}"
+			current_record_string = f"{record.level_name or 0}, {record.rating or 0}, {record.rating_sum or 0}, {record.auto or 0}, {record.demon or 0}, {record.stars or 0}, {record.demon_type or 0}, {record.level_version or 0}, {record.real_user_record_id or 0}, {record.game_version or 0}, {record.level_string_id or 0}, {record.coins or 0}, {record.description or 0}, {record.song_id or 0}, {record.official_song or 0}, {record.feature_score or 0}, {record.epic or 0}, {record.password or 0}, {record.two_player or 0}, {record.objects_count or 0}, {record.extra_string or 0}, {record.original or 0}, {record.daily_id or 0}, {record.timestamp or 0}, {record.song_ids}, {record.sfx_ids}"
 			if (record.downloads or 0) > highest_downloads:
 				highest_downloads_record = record
 				highest_downloads = record.downloads or 0
