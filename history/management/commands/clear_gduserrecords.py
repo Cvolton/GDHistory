1003c5d43c809d6b3fd63019c70a2f33215c23fd
Cvolton <cvolton@cvolton.eu>
2025-02-25 11:19:42 +0100

    print progress in clear_gduserrecords

--- history/management/commands/clear_gduserrecords.py
+++ history/management/commands/clear_gduserrecords.py
@@ -6,7 +6,9 @@ class Command(BaseCommand):
 	help = 'Deletes all GDUserRecords'
 
 	def handle(self, *args, **options):
+		i = 0
 		while GDUserRecord.objects.all()[:1].count() > 0:
+			print(f"Deleting 100000 records, iteration {i}")
 			level_records = GDUserRecord.objects.all()[:100000]
 			for record in level_records:
 				record.delete()
