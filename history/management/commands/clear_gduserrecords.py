74137d5cb592b4d84d35f1746112b737b19e9c52
Cvolton <cvolton@cvolton.eu>
2025-02-25 11:53:20 +0100

    make record_count a positional argument

--- history/management/commands/clear_gduserrecords.py
+++ history/management/commands/clear_gduserrecords.py
@@ -5,9 +5,12 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 	help = 'Deletes all GDUserRecords'
 
+	def add_arguments(self, parser):
+		parser.add_argument('levels', nargs='+')
+
 	def handle(self, *args, **options):
+		record_count = options['levels']
 		i = 0
 		while GDUserRecord.objects.all()[:1].count() > 0:
-			record_count = 100000
 			print(f"Deleting {record_count} records, iteration {i}")
 			level_records = GDUserRecord.objects.filter(pk__lt=GDUserRecord.objects.first().pk + record_count).delete()
