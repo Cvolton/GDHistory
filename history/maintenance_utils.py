e654d2de61b4d0eb559aa7b972a11655491cdd9c
Cvolton <cvolton@cvolton.eu>
2025-02-28 09:46:31 +0100

    batch search cache updating more

--- history/maintenance_utils.py
+++ history/maintenance_utils.py
@@ -32,17 +32,23 @@ def do_is_public_updating(records):
 	LevelRecord.objects.bulk_update(records, ['cache_is_public'], batch_size=1000)
 
 def do_search_cache_updating(records, status):
-	records = records[:100000]
-	record_count = records.count()
-	i = 1
-	for record in records:
-		print(f"{i} / {record_count} - Updating {record.online_id}")
-		record.cache_search_available = status
-		record.cache_needs_search_update = True
-		#record.save()
-		i += 1
+	records_max = 5000
 
-	Level.objects.bulk_update(records, ['cache_search_available', 'cache_needs_search_update'], batch_size=1000)
+	while True:
+		records = records[:records_max]
+		record_count = records.count()
+		i = 1
+		for record in records:
+			print(f"{i} / {record_count} - Updating {record.online_id}")
+			record.cache_search_available = status
+			record.cache_needs_search_update = True
+			#record.save()
+			i += 1
+
+		Level.objects.bulk_update(records, ['cache_search_available', 'cache_needs_search_update'], batch_size=1000)
+
+		if len(records) < records_max:
+			break
 
 def update_cached_fields():
 	do_is_public_updating(LevelRecord.objects.prefetch_related('level').filter(cache_is_public=False).exclude(level__is_public=False))
