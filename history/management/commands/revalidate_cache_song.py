87cd72820d0900cd750ac01faea64661b7b2d648
Cvolton <cvolton@cvolton.eu>
2025-03-03 20:43:46 +0100

    fix revalidate_cache_song to only revalidate needs revalidation

--- history/management/commands/revalidate_cache_song.py
+++ history/management/commands/revalidate_cache_song.py
@@ -7,11 +7,10 @@ class Command(BaseCommand):
 	help = 'Revalidates level cache'
 
 	def handle(self, *args, **options):
-		songs = Song.objects.all().prefetch_related('songrecord_set__save_file')
+		songs = Song.objects.filter(cache_needs_revalidation=True)
 		song_count = songs.count()
 		for i in range(0,song_count):
-			song = songs[i:i+1]
-			song = song[0]
+			song = songs[0:1][0]
 			print(f"{i} / {song_count} - Updating {song.online_id}")
 			song.revalidate_cache()
 			i += 1
