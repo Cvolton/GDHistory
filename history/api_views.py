50806db293006e2365290f0750f7905a371b986d
Cvolton <cvolton@cvolton.eu>
2025-07-17 10:52:19 +0200

    remove debug print of blacklisted user ids

--- history/api_views.py
+++ history/api_views.py
@@ -49,8 +49,6 @@ def level_info(request, online_id=None, view_mode="normal"):
 	if level is None or (not (request.user.is_authenticated and request.user.is_superuser) and not (level.is_public or int(online_id) < utils.get_level_id_within_window())) or level.levelrecord_set.count() == 0 or level.cache_user_id in utils.get_blacklisted_userids():
 		return JsonResponse({'success': False}, status=404)
 
-	print(utils.get_blacklisted_userids())
-
 	all_levels = level.levelrecord_set.filter(is_invalid=False)
 
 	response = level.get_serialized_base()
