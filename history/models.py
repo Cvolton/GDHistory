4a0f3150d9af2092d18f5fb34c8aa958082f00dc
Cvolton <clubpenguinmisa@gmail.com>
2026-02-25 20:34:15 +0100

    actually this index will probably be better

--- history/models.py
+++ history/models.py
@@ -1036,7 +1036,7 @@ class CommentDateEstimation(models.Model):
 
 	class Meta:
 		indexes = [
-			models.Index(fields=['type', 'range_id', 'comment_id'], name='type_range_comment')
+			models.Index(fields=['type', 'range_id', '-estimation'], name='type_range_est_desc_idx')
 		]
 
 class LevelDateEstimation(models.Model):
