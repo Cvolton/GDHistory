0acfd10c9a84cccd3f076b8be024123db1a5de6c
Cvolton <clubpenguinmisa@gmail.com>
2026-06-03 02:50:31 +0200

    optimize comment date estimator db index

--- history/api_views.py
+++ history/api_views.py
@@ -161,25 +161,37 @@ def comment_date_estimation(request, level_id, comment_id, estimation_type="leve
 	range_id = range_funcs.get(estimation_type, lambda level_id: 0)(level_id)
 	comment_id = int(comment_id)
 
-	low = CommentDateEstimation.objects.raw(
-		"""
-		SELECT * FROM history_commentdateestimation
-		FORCE INDEX (type_range_est_desc_idx)
-		WHERE type = %s AND range_id = %s AND comment_id <= %s
-		ORDER BY estimation DESC
-		LIMIT 1
-		""",
-		[estimation_type, range_id, comment_id]
-	)
-	high = CommentDateEstimation.objects.raw(
-		"""
-		SELECT * FROM history_commentdateestimation
-		FORCE INDEX (type_range_est_comment_idx)
-		WHERE type = %s AND range_id = %s AND comment_id >= %s
-		ORDER BY estimation ASC LIMIT 1;
-		""",
-		[estimation_type, range_id, comment_id]
-	)
+	# low = CommentDateEstimation.objects.raw(
+	# 	"""
+	# 	SELECT * FROM history_commentdateestimation
+	# 	FORCE INDEX (type_range_est_desc_idx)
+	# 	WHERE type = %s AND range_id = %s AND comment_id <= %s
+	# 	ORDER BY estimation DESC
+	# 	LIMIT 1
+	# 	""",
+	# 	[estimation_type, range_id, comment_id]
+	# )
+	# high = CommentDateEstimation.objects.raw(
+	# 	"""
+	# 	SELECT * FROM history_commentdateestimation
+	# 	FORCE INDEX (type_range_est_comment_idx)
+	# 	WHERE type = %s AND range_id = %s AND comment_id >= %s
+	# 	ORDER BY estimation ASC LIMIT 1;
+	# 	""",
+	# 	[estimation_type, range_id, comment_id]
+	# )
+
+	low = CommentDateEstimation.objects.filter(
+		type=estimation_type,
+		range_id=range_id,
+		comment_id__lte=comment_id
+	).order_by('-comment_id')[:1]
+
+	high = CommentDateEstimation.objects.filter(
+		type=estimation_type,
+		range_id=range_id,
+		comment_id__gte=comment_id
+	).order_by('comment_id')[:1]
 
 	#if low: low = CommentDateEstimation.objects.filter(type=estimation_type, range_id=range_id, estimation=low[0].estimation).order_by('estimation')
 	#if high: high = CommentDateEstimation.objects.filter(type=estimation_type, range_id=range_id, estimation=high[0].estimation).order_by('estimation')
