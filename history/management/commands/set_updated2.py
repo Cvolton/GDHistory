ab13c3c12e7132a382c25175fc6407b70a46f0d3
Cvolton <cvolton@cvolton.eu>
2026-06-04 22:41:37 +0200

    add set_updated2

---
+++ history/management/commands/set_updated2.py
@@ -0,0 +1,30 @@
+from history.models import Level
+import history.utils
+import json
+import math
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Min
+from django.db.models.functions import Coalesce
+
+
+class Command(BaseCommand):
+	help = 'set updated2'
+
+	def handle(self, *args, **options):
+
+		while True:
+			print('getting levels')
+			objects = Level.objects.filter(is_deleted=True, cache_needs_updating=True)[:10000]
+
+			for object in objects:
+				object.cache_needs_updating = False
+				object.cache_needs_updating2 = True
+
+			print('updating levels')
+			Level.objects.bulk_update(objects, ['cache_needs_updating', 'cache_needs_updating2'], batch_size=1000)
+
+			if len(objects) < 10000:
+				print('done')
+				break
+		
